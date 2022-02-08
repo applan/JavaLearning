738164d5cd5ed98b6db7654d88331533d186e4b8
applan <48544100+applan@users.noreply.github.com>
2022-02-09 00:26:33 +0900

    day03 복습 중 실수(값비교해야하는데 index로 비교중)

--- review/src/review/day03/Main_220208.java
+++ review/src/review/day03/Main_220208.java
@@ -1,9 +1,48 @@
 package review.day03;
 
+import util.PrintUtil;
+import util.SwapUtil;
+
 public class Main_220208 {
 
 	public static void main(String[] args) {
 		
+		int[] arr = {3, 9, 4, 7, 5, 0, 1, 6, 8, 2};
+		
+		quickSort(arr);
+		
+		PrintUtil.print(arr);
+		
+	}
+	
+	private static void quickSort(int[] arr) {
+		quickSortAction(arr, 0, arr.length-1);
+	}
+	
+	private static void quickSortAction(int[] arr, int start, int end) {
+		int part2 = partition(arr, start, end);
+		System.out.println(part2);
+		if(start < part2 - 1) {
+			quickSortAction(arr, start, part2 - 1);
+		}
+		if(end > part2) {
+			quickSortAction(arr, part2, end);
+		}
+		System.out.println(part2);
+	}
+	
+	private static int partition(int[] arr, int start, int end) {
+		int pivot = arr[(start + end) / 2];
+		while(start <= end) {
+			while(start < pivot) start++;
+			while(end > pivot) end--;
+			if(start <= end) {
+				SwapUtil.swap(arr, start, end);
+				start++;
+				end--;
+			}
+		}
+		return start;
 	}
 	
 }
