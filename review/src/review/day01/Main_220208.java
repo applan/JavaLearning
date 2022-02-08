fbc3eecc7a2148c002510e140291ece25d4b28e1
applan <48544100+applan@users.noreply.github.com>
2022-02-08 23:50:38 +0900

    day01 복습 재귀 완료

--- review/src/review/day01/Main_220208.java
+++ review/src/review/day01/Main_220208.java
@@ -11,18 +11,35 @@ public class Main_220208 {
 		// 버블 정렬용
 		int[] arr = {3, 5, 2, 4, 1};
 		
-		int length = arr.length - 1;
+//		int length = arr.length - 1;
 		
-		for(int i=0; i<length; i++) {
-			for(int j=0; j<length-i; j++) {
-				// 오른쪽이 큰 값이라는걸 항상 명심
-				if(arr[j] > arr[j+1]) {
-					SwapUtil.swap(arr, j, j+1);
-				}
-			}
-		}
+//		for(int i=0; i<length; i++) {
+//			for(int j=0; j<length-i; j++) {
+//				// 오른쪽이 큰 값이라는걸 항상 명심
+//				if(arr[j] > arr[j+1]) {
+//					SwapUtil.swap(arr, j, j+1);
+//				}
+//			}
+//		}
+		
+		bubbleSort(arr);
 		
 		PrintUtil.print(arr);
 		
+	}
+	
+	private static void bubbleSort(int[] arr) {
+		bubbleSortAction(arr, arr.length-1);
+	}
+	
+	private static void bubbleSortAction(int[] arr, int last) {
+		if(last > 0) {
+			for(int i=0; i<last; i++) {
+				if(arr[i] > arr[i+1]) {
+					SwapUtil.swap(arr, i, i+1);
+				}
+			}
+			bubbleSortAction(arr, last -1);
+		}
 	}
 }
