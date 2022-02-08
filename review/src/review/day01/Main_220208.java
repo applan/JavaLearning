4c125ad654737993fec67ac62b035b84225d0c5b
applan <48544100+applan@users.noreply.github.com>
2022-02-09 00:05:19 +0900

    day02 반복문, 재귀 복습 완료

--- review/src/review/day01/Main_220208.java
+++ review/src/review/day01/Main_220208.java
@@ -6,27 +6,27 @@ import util.SwapUtil;
 // 버블 정렬 복습 2022-02-08
 public class Main_220208 {
 
-	public static void main(String[] args) {
-		
-		// 버블 정렬용
-		int[] arr = {3, 5, 2, 4, 1};
-		
-//		int length = arr.length - 1;
-		
-//		for(int i=0; i<length; i++) {
-//			for(int j=0; j<length-i; j++) {
-//				// 오른쪽이 큰 값이라는걸 항상 명심
-//				if(arr[j] > arr[j+1]) {
-//					SwapUtil.swap(arr, j, j+1);
-//				}
-//			}
-//		}
-		
-		bubbleSort(arr);
-		
-		PrintUtil.print(arr);
-		
-	}
+//	public static void main(String[] args) {
+//		
+//		// 버블 정렬용
+//		int[] arr = {3, 5, 2, 4, 1};
+//		
+////		int length = arr.length - 1;
+//		
+////		for(int i=0; i<length; i++) {
+////			for(int j=0; j<length-i; j++) {
+////				// 오른쪽이 큰 값이라는걸 항상 명심
+////				if(arr[j] > arr[j+1]) {
+////					SwapUtil.swap(arr, j, j+1);
+////				}
+////			}
+////		}
+//		
+//		bubbleSort(arr);
+//		
+//		PrintUtil.print(arr);
+//		
+//	}
 	
 	private static void bubbleSort(int[] arr) {
 		bubbleSortAction(arr, arr.length-1);
--- review/src/review/day02/Main_220208.java
+++ review/src/review/day02/Main_220208.java
@@ -1,10 +1,45 @@
 package review.day02;
 
-// 
+import util.PrintUtil;
+import util.SwapUtil;
+
+// 선택 정렬
 public class Main_220208 {
 
 	public static void main(String[] args) {
 		
+		int[] arr = {3, 5, 4, 2, 1};
+		
+//		for(int i=0; i<arr.length; i++) {
+//			int min = i;
+//			for(int j=i+1; j<arr.length; j++) {
+//				if(arr[min] > arr[j]) {
+//					min = j;
+//				}
+//			}
+//			SwapUtil.swap(arr, i, min);
+//		}
+		
+		SectionSort(arr);
+		PrintUtil.print(arr);
+		
+	}
+	
+	private static void SectionSort(int[] arr) {
+		SectionSortAction(arr, 0);
+	}
+	
+	private static void SectionSortAction(int[] arr, int current) {
+		if(current < arr.length -1 ) {
+			int min = current;
+			for(int i=current + 1; i<arr.length; i++) {
+				if(arr[min] > arr[i]) {
+					min = i;
+				}
+			}
+			SwapUtil.swap(arr, current, min);
+			SectionSortAction(arr, current + 1);
+		}
 	}
 	
 }
