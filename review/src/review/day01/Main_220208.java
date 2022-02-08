bcfddf6664e819c02060b4c79b121b4d62f8cc8b
applan <48544100+applan@users.noreply.github.com>
2022-02-08 23:44:57 +0900

    day01 복습반복문 완료

--- review/src/review/day01/Main_220208.java
+++ review/src/review/day01/Main_220208.java
@@ -11,16 +11,15 @@ public class Main_220208 {
 		// 버블 정렬용
 		int[] arr = {3, 5, 2, 4, 1};
 		
-		
 		int length = arr.length - 1;
-		for(int i=0; i<length - 1; i++) {
-			
-			for (int j=0; j<length; j++) {
+		
+		for(int i=0; i<length; i++) {
+			for(int j=0; j<length-i; j++) {
+				// 오른쪽이 큰 값이라는걸 항상 명심
 				if(arr[j] > arr[j+1]) {
 					SwapUtil.swap(arr, j, j+1);
 				}
 			}
-			
 		}
 		
 		PrintUtil.print(arr);
