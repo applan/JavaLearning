889fcc9a18234f468b425012075277b62b52fc9d
applan <48544100+applan@users.noreply.github.com>
2022-02-10 22:29:59 +0900

    퀵정렬 복습 완료 ( 재귀, 반복문)

---
+++ review/src/review/day03/Main_220210.java
@@ -0,0 +1,46 @@
+package review.day03;
+
+import util.PrintUtil;
+import util.SwapUtil;
+
+// 퀵 정렬
+public class Main_220210 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 9, 3, 4, 5, 0, 7, 8, 6, 2};
+		
+		quickSort(arr);
+		
+		PrintUtil.print(arr);
+		
+	}
+	
+	private static void quickSort(int[] arr) {
+		quickSortAction(arr, 0, arr.length-1);
+	}
+	
+	private static void quickSortAction(int[] arr, int start, int end) {
+		int part = partition(arr, start, end);
+		if(start < part - 1) {
+			quickSortAction(arr, start, part - 1);
+		}
+		if(end > part) {
+			quickSortAction(arr, part, end);
+		}
+	}
+	
+	private static int partition(int[] arr, int start, int end) {
+		int pivot = arr[(start+end) / 2];
+		while(start <= end) {
+			while(arr[start] < pivot) start++;
+			while(arr[end] > pivot) end--;
+			if(start <= end) {
+				SwapUtil.swap(arr, start, end);
+				start ++;
+				end --;
+			}
+		}
+		return start;
+	}
+}
